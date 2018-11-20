0e3ae708af762fbc6ce90539a04d00e0be5c7cc9
Justin R. Jent <jentjr@gmail.com>
2018-11-20 18:28:48 -0500

    TESTS: add tests for Uflow

---
+++ tests/test_uflow.py
@@ -0,0 +1,43 @@
+from timml import *
+import numpy as np
+
+
+ml = ModelMaq(kaq=[1, 2], z=[10, 5, 4, 0], c=20)
+
+w = HeadWell(ml, xw=10, yw=0, hw=-1, rw=0.1, layers=[0])
+rf = Constant(ml, xr=0, yr=0, hr=1)
+uf = Uflow(ml, 0.01, 45)
+
+ml.solve()
+xg, yg = np.linspace(-50, 50, 50), np.linspace(-50, 50, 50)
+h = ml.headgrid(xg, yg)
+
+print("xg, yg:", xg, yg)
+print("h:", h)
+
+
+#####################
+ml = ModelMaq(kaq=[1, 2], z=[10, 5, 4, 0], c=20)
+
+xy = [(-5, -5), (5, -3), (3, 5), (-4, 4)]
+p1 = PolygonInhomMaq(
+    ml,
+    xy=xy,
+    kaq=[0.2, 8],
+    z=[11, 10, 5, 4, 0],
+    c=[10, 50],
+    topboundary="semi",
+    hstar=1.0,
+    order=5,
+    ndeg=3,
+)
+
+rf = Constant(ml, xr=50, yr=0, hr=1)
+uf = Uflow(ml, 0.01, 45)
+
+ml.solve()
+xg, yg = np.linspace(-10, 10, 50), np.linspace(-10, 10, 50)
+h = ml.headgrid(xg, yg)
+
+print("xg, yg:", xg, yg)
+print("h:", h)
