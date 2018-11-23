315f0d5f4e561f73a24b0e5937495e0497ebb954
Justin Jent <jentjr@gmail.com>
2018-11-23 18:13:11 -0500

    more cleanup of inhomogeneity test

--- tests/test_inhomogeneity.py
+++ tests/test_inhomogeneity.py
@@ -4,6 +4,8 @@ from numpy.testing import assert_allclose
 def test_xy_vertices():
     # model with vertices entered counter-clockwise
     ccxy = [(-1, -1), (1, -1), (1, 1), (-1, 1)]
+    assert inhomogeneity._ispolycw(ccxy) == False
+
     ml1 =  ModelMaq(z=[1, 0, -1], c=[100], topboundary="semi", hstar=1.0)
     inhom = PolygonInhomMaq(ml1, ccxy)
     w = Well(ml1)
@@ -12,6 +14,8 @@ def test_xy_vertices():
     
     # model with vertices entered clockwise
     cwxy = [(-1, -1), (-1, 1), (1, 1), (1, -1)]
+    assert inhomogeneity._ispolycw(cwxy) == True
+
     ml2 =  ModelMaq(z=[1, 0, -1], c=[100], topboundary="semi", hstar=1.0)
     inhom = PolygonInhomMaq(ml2, xy=cwxy)
     w = Well(ml2)
